a33fab376b6195b9aed6775cc6217cba2993de4d
sanjeep123 <120095970+sanjeep123@users.noreply.github.com>
2022-12-26 02:44:32 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    // let i;
+    // const arrows = document.querySelectorAll("#arrow");
+    // let movieLists = document.querySelectorAll(".movie-list");
+    // arrows.forEach((arrow,i)=> {
+    //     arrow.addEventListener("click",()=> {
+    //     movieLists[i].style.transform = `translateX(${
+    //                 movieLists[i].computedStyleMap.get("transform")[0].x.value
+    //                  -300}px)`;
+    //     });
+    // });
+
+    
